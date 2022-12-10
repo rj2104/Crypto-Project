29dbd2d2bb369dbc7fa7dacdb1c5fabbc6bed961
Rahul Jain <104311833+rj2104@users.noreply.github.com>
2022-12-10 14:56:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+
+from PassHub_sync import cli
+from PassHub_sync.ui import set_mode
+
+if __name__ == "__main__":
+    args = sys.argv
+    argc = len(args)
+    continuous = argc == 3 and args[2] == "c"
+
+    if argc > 1 and args[1] not in ["add", "remove", "update"] or argc == 3 and args[2] != "c":
+        cli.usage()
+        print(">>> Invalid Option! <<<")
+        sys.exit(2)
+
+    if argc > 3:
+        cli.usage()
+        print(">>> Too Many Arguments! <<<")
+        sys.exit(2)
+
+    if argc != 1 and args[1] == "add":
+        set_mode("Save Mode")
+        if continuous:
+            cli.add_profiles()
+        else:
+            cli.add_profile()
+
+    elif argc != 1 and args[1] == "remove":
+        set_mode("Delete Mode")
+        if continuous:
+            cli.remove_profiles()
+        else:
+            cli.remove_profile()
+
+    elif argc != 1 and args[1] == "update":
+        set_mode("Update Mode")
+        if continuous:
+            cli.usage()
+            print(">>> Unsupported Argument `c` <<<")
+            sys.exit(2)
+        else:
+            cli.update_password()
+
+    elif argc != 1 and args[1] == "help":
+        cli.usage()
+
+    else:
+        set_mode("View Mode")
+        cli.get_password()
